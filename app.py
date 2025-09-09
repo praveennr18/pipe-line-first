6c6bf4c32a606c2dd5198e8eed2ae960e4d4c86a
praveen <praveen@pmail.com>
2025-09-09 18:42:32 +0530

    intentionally wrong

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# def add(a, b):
+#     return a + b
+
 def add(a, b):
-    return a + b
+    return a + b + 1  # ❌ intentionally wrong
 
 if __name__ == "__main__":   # 👈 fixed typo (_name_ → __name__)
     print(add(1, 2))
